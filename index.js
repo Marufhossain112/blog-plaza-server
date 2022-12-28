4e8beb1a28e81931aa440f5ebdfc11ff85067b7a
maruf <dev.maruf112@gmail.com>
2022-12-28 09:25:11 +0600

    create api for update blog

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
   res.send("I am running on the home of the Server.");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@cluster0.efpjwcu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -37,6 +37,34 @@ async function run() {
       const query = {};
       const cursor = await addBlogCollections.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    // get specific data
+    app.get("/blogDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+      const result = await addBlogCollections.findOne(query);
+      res.send(result);
+    });
+    // update specific data
+    app.put("/updateBlog/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const blog = req.body;
+      const option = { upsert: true };
+      const updatedBlog = {
+        $set: {
+          title: blog.title,
+          tags: blog.tags,
+          blogText: blog.blogText,
+        },
+      };
+      const result = await addBlogCollections.updateOne(
+        filter,
+        updatedBlog,
+        option
+      );
       res.send(result);
     });
   } finally {
