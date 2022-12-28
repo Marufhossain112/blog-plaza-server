f81078c47dc0faa01dece27887c1f52f04afd89c
maruf <dev.maruf112@gmail.com>
2022-12-28 10:39:14 +0600

    create delete api

--- index.js
+++ index.js
@@ -67,6 +67,13 @@ async function run() {
       );
       res.send(result);
     });
+    // delete blog data
+    app.delete("/deleteBlog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await addBlogCollections.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
